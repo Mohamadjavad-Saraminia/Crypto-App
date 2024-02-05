c52a09d627632563e87f36b69b7f193b6d7800a7
Mj Saraminia <mjsaraminiadeveloped@gmail.com>
2024-02-05 04:44:10 +0330

    Tabelrow component in tabelcoins map && Destrucring Coins

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -17,34 +17,39 @@ function TableCoin({ coins }) {
                     </tr>
                 </thead>
 
+                {/* به جای ایجاد یک component مجزا اومدیم پایین منطق کد رو نوشتیم و درون خود پیچ نوشتیمش */}
                 <tbody>
                     {coins.map(coins =>
-
-                        <tr key={coins.id}>
-
-                            <td>
-                                <div>
-                                    <img src={coins.image} alt="alt" />
-                                    <span>{coins.symbol.toUpperCase()}</span>
-                                    {/* symbol be horoofe bozorg */}
-                                </div>
-                            </td>
-                            <td>{coins.name}</td>
-                            {/* قیمت جاری - toLocaleString() میاد 3رقم 3رقم جدا میکنه اعداد رو */}
-                            <td>${coins.current_price.toLocaleString()}</td>
-                            {/* price-change  درصد تغییرات | toFixed(2) یعنی تا 2 رقم اعشار نشون بده*/}
-                            <td>{coins.price_change_percentage_24h.toFixed(2)}%</td>
-                            <td>{coins.total_volume.toLocaleString()}</td>
-                            {/* Chart + or - ro moshakhas kardim */}
-                            <td> <img src={coins.price_change_percentage_24h > 0 ? chartUp : chartDown} alt={coins.name} /></td>
-                        </tr>)}
+                        <TabelRow coins={coins} key={coins.id} />
+                    )}
                 </tbody>
             </table>
         </div>
     )
-
-
-
 }
 
 export default TableCoin
+
+// destructure coins
+// یک component دیگه درون tabelcoins به نام Tabelrowساختیم تا از تکرار جلوگیری کنیم
+const TabelRow = ({ coins: { id, name, image, symbol, total_volume, current_price, price_change_percentage_24h: price_change }, }) => {
+    return (
+        <tr key={id}>
+            <td>
+                <div>
+                    <img src={image} alt="alt" />
+                    <span>{symbol.toUpperCase()}</span>
+                    {/* symbol be horoofe bozorg */}
+                </div>
+            </td>
+            <td>{name}</td>
+            {/* قیمت جاری - toLocaleString() میاد 3رقم 3رقم جدا میکنه اعداد رو */}
+            <td>${current_price.toLocaleString()}</td>
+            {/* price-change  درصد تغییرات | toFixed(2) یعنی تا 2 رقم اعشار نشون بده*/}
+            <td>{price_change.toFixed(2)}%</td>
+            <td>{total_volume.toLocaleString()}</td>
+            {/* Chart + or - ro moshakhas kardim */}
+            <td> <img src={price_change > 0 ? chartUp : chartDown} alt={name} /></td>
+        </tr>
+    )
+}
