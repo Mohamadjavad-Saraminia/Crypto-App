9c9b484ea7d95ef868fe7dac4817c9bb4c4c7e63
Mj Saraminia <mjsaraminiadeveloped@gmail.com>
2024-02-27 15:05:10 +0330

    SearchBox Css Style

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { searchCoin } from "../../Services/cryptoApi";
 import { RotatingLines } from "react-loader-spinner";
+import styles from "../modules/Search.module.css";
 
 function Search({ currency, setCurrency, }) {
     //آنچه که کاربر وارد میکند
@@ -46,14 +47,14 @@ function Search({ currency, setCurrency, }) {
 
 
     return (
-        <div>
+        <div className={styles.searchBox}>
             <input type="text" placeholder="Search" value={text} onChange={e => setText(e.target.value)} />
             <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
                 <option value="usd">USD</option>
                 <option value="eur">EUR</option>
                 <option value="jpy">JPY</option>
             </select>
-            <div>
+            <div className={styles.searchResult}>
                 <ul>
                     {isloading && <RotatingLines width="50px" height="50px" strokeWidth="3" strokeColor="#3874ff" />}
                     {coins.map(coin => <li key={coin.id}>
